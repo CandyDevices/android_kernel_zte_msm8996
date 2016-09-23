d8d56eb1e916e7f346a21257cbff2a53ab9a282a
Eliminater74 <eliminater74@gmail.com>
2016-09-23 00:09:54 -0400

    cpufreq: Add Chill cpu gov
    * Fixed GCC Error,
    * You will need display State API as well
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>
    
    Change-Id: I8590ace5ba60bd728c60f06288a9d6018683037a
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- drivers/cpufreq/cpufreq_governor.h
+++ drivers/cpufreq/cpufreq_governor.h
@@ -202,9 +202,13 @@ struct cs_dbs_tuners {
 	unsigned int sampling_down_factor;
 	unsigned int up_threshold;
 	unsigned int down_threshold;
+	unsigned int down_threshold_suspended;
 	unsigned int freq_step;
     unsigned int twostep_counter;
     u64 twostep_time;
+	unsigned int sleep_depth;
+	unsigned int boost_enabled;
+	unsigned int boost_count;
 };
 
 struct ex_dbs_tuners {
